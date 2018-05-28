19e5bfd4caf31d33f4b925040528beb0bff3bd89
hoytchang <hoyt.chang@gmail.com>
2018-05-28 00:23:49 -0400

    clean up code
    removed debugging prints, added comments, replaced try except with isinstance

--- cross_platform_recommend.py
+++ cross_platform_recommend.py
@@ -26,21 +26,22 @@ sr = 'bitcoin'
 #For each submission get the title, body, and comment.  For each comment, get the body.  Store everything in raw_text.
 raw_text = ""
 subreddit = reddit.subreddit(sr)
-top_subreddit = subreddit.top(limit=3)
+print("Getting data from "+subreddit._path)
+top_subreddit = subreddit.top(limit=3) #use low limit for debugging, high limit for more data
+count_submission = 0
+count_comment = 0
 for submission in top_subreddit:
-	print('\n   title = ' + submission.title) #some times gives error UnicodeEncodeError: 'charmap' codec can't encode character
-	raw_text += " " + submission.title
-	print('    body = ' + submission.selftext)
-	raw_text += " " + submission.selftext
+	raw_text += " " + submission.title #add title
+	raw_text += " " + submission.selftext #add body
+	count_submission += 1
 	comments = submission.comments.list()
-	max_comments = 1000
+	max_comments = 1000 #put a cap on how many comments per submission
 	n_comments = min(max_comments, len(comments))
 	for i in range(n_comments):
-		try:
-			print('      comment ' + str(i) + ' = ' + comments[i].body)
-			raw_text += " " + comments[i].body
-		except:
-			pass
+		if not isinstance(comments[i], praw.models.reddit.more.MoreComments): #avoid MoreComments
+			raw_text += " " + comments[i].body #add comment
+			count_comment += 1
+print("Collected data from " + str(count_submission) + " submissions and " + str(count_comment) + " comments.")
 
 #Use nltk to convert string into tokens (words and puncuation)
 tokens = word_tokenize(raw_text)
@@ -52,15 +53,13 @@ fdist = FreqDist(tokens)
 short_words = []
 for i in fdist:
 	if len(i) < 3:
-		print('to be removed: ' + i)
 		short_words.append(i)
 for s in short_words:
-	print('removing: '+s)
 	fdist.pop(s)
 
 #Remove common but useless tokens 
 stop = set(stopwords.words('english')) #get pre-defined stop words
-additional_stop = ['https','n\'t', 'like', 'buy'] #add additional stop words
+additional_stop = ['https','http','n\'t','The','This','That','...'] #add additional stop words
 for a in additional_stop:
 	stop.add(a) 
 for s in stop:
@@ -68,5 +67,13 @@ for s in stop:
 		fdist.pop(s)
 	except:
 		pass
+
 #needs some work here to stem the tokens
 
+
+#print results of nltk
+print("The 50 most commonly used words in this subreddit: ")
+for f in fdist.most_common(50):
+	print(f)
+
+#search youtube
