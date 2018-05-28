f4cd4316f3d4e2c67be389a23d3d44aa33bf1f4e
hoytchang <hoyt.chang@gmail.com>
2018-05-28 12:21:57 -0400

    added youtube search
    added youtube search functionality

--- cross_platform_recommend.py
+++ cross_platform_recommend.py
@@ -5,6 +5,8 @@ from myRedditKeys import myRedditKeys
 from nltk import word_tokenize
 from nltk import FreqDist
 from nltk.corpus import stopwords
+import requests
+from bs4 import BeautifulSoup as bs
 
 #Set encoding to UTF-8 to avoid Unicode Encode Error
 if sys.platform == "win32":
@@ -68,12 +70,40 @@ for s in stop:
 	except:
 		pass
 
-#needs some work here to stem the tokens
+#Needs some work here to stem the tokens
 
 
-#print results of nltk
+#Print results of nltk
 print("The 50 most commonly used words in this subreddit: ")
 for f in fdist.most_common(50):
 	print(f)
 
-#search youtube
+#Search youtube.  
+#For now, just search the subreddit name.  
+#Later, we can search all the tokens from the subreddit, and also search the youtube comments to find better matches.
+base = "https://www.youtube.com/results?search_query="
+qstring = sr
+r = requests.get(base+qstring)
+page = r.text
+
+#parse using beautiful soup
+soup = bs(page,'html.parser')
+
+#Find all <a> tags because they define hyper-links.  The parameter href is the link.
+#Only find <a> tags that are classified as YouTube User Interface XML Tile Links: yt-uix-tile-link
+vids = soup.findAll('a',attrs={'class':'yt-uix-tile-link'})
+
+#Extract weblink for each tile using the href property.  Append to list.
+videolist = []
+videoname = []
+for v in vids:
+	tmp = 'https://www.youtube.com' + v['href']
+	videolist.append(tmp)
+	videoname.append(v['title'])
+
+#Print search results
+print('Recommended videos: ')
+for i in range(len(videolist)):
+	if 'googleads' not in videolist[i]: #avoid google ads
+		print('\n' + videoname[i][0:60]) #title of video, truncated
+		print(videolist[i]) #url of video
